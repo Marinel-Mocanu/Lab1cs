82ff8fef5f0f9522104419d85871814ae7546aa2
Marinel-Mocanu <marinel.mocanu@student.usv.ro>
2026-03-09 21:18:01 +0200

    Muzic_Store Update
    Am adaugat clasele pe care le-am considerat cele mai necesare. Pe viitor doresc sa extind aceste clase in functie de necesitatile pe care o sa le intalnesc in timpul realizarii proiectului.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿public class Instrument
+{
+    public int ID { get; set; };
+    public string Name { get; set; };
+    public string Brand { get; set; };
+    public Instrument_Category Category { get; set; };
+    public double Price { get; set; };
+    public string Description { get; set; };
+    public int Quantity { get; set; };
+    public double Discount { get; set; };
+    public double Final_Price()
+    {
+        return Price - (Price - Discount / 100);
+    }
+}
+public class Client
+{
+
+    public int ID { get; set; };
+    public string Name { get; set; };
+    public string Email { get; set; };
+    public string Password { get; set; };
+}
+public class Account_Clients
+{
+    public void Create_Account()
+    {
+
+    }
+}
+public enum Instrument_Category
+{
+    Guitars,
+    Drums,
+    Keyboards,
+    Mics,
+    Amps,
+    Synths,
+    Cables,
+    Strings,
+    Accessories
+}
+class Store
+{
+    public void Add_Instrument(Instrument instrument)
+    {
+
+    }
+    public void Delete_Instrument()
+    {
+
+    }
+    public void Update_Instrument(Instrument instrument)
+    { 
+
+    }
+    public Instrument Search_Instrument(string name)
+    {
+
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+
+}
